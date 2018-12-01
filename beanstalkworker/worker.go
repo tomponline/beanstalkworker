999e778d19ad6f7990be718057edf22869b44e97
Neville Cain <neville.cain@infinitycloud.com>
2018-12-01 23:13:50 +0000

    if user enters unknown action, ensure log message is using Release and not the custom action entered by the user

--- beanstalkworker/worker.go
+++ beanstalkworker/worker.go
@@ -97,6 +97,10 @@ func (w *Worker) Run(ctx context.Context) {
 func (w *Worker) SetUnmarshalErrorAction(action string) {
 	// If this action is different than Delete, Bury or Release, the last one will be chosen
 	// as the default action in case of an unmarshal error, via the method job.unmarshalErrorHandling.
+	if action != ActionDeleteJob && action != ActionBuryJob {
+		w.unmarshalErrorAction = ActionReleaseJob // By safety only and to keep log message conistent with the action
+		return
+	}
 	w.unmarshalErrorAction = action
 }
 
